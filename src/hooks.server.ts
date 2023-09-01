ecd6caa9b2a823479ba0e85e78933026e176c148
Joel Helbling <helblingjoel@gmail.com>
2023-09-01 16:53:05 +0100

    Only send to Sentry when in prod

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,15 +2,18 @@ import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
 import type { HandleServerError } from '@sveltejs/kit';
 import crypto from 'crypto';
+import { SENTRY_DSN } from '$env/static/private';
 
 Sentry.init({
-	dsn: 'https://7772cafd20f486c28a438b4544b2b8f8@o4505790206705664.ingest.sentry.io/4505790208278528',
+	dsn: SENTRY_DSN,
 	tracesSampleRate: 1
 });
 
 export const handleError: HandleServerError = async ({ error, event }) => {
 	const errorId = crypto.randomUUID();
-	Sentry.captureException(error, { extra: { event, errorId } });
+	if (process.env.NODE_ENV === 'production') {
+		Sentry.captureException(error, { extra: { event, errorId } });
+	}
 
 	console.dir({
 		level: 'ERROR',
