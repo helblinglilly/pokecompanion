98b5522f9801416174ee53dac26840b5b7277a91
Joel Helbling <helblingjoel@gmail.com>
2023-09-25 20:45:01 +0100

    Tidy

--- src/params/pokedexid.ts
+++ src/params/pokedexid.ts
@@ -2,8 +2,5 @@ import type { ParamMatcher } from '@sveltejs/kit';
 import { lastPokedexEntry } from '$lib/domain';
 
 export const match: ParamMatcher = (param) => {
-	if (Number(param) >= 1 && Number(param) <= lastPokedexEntry) {
-		return true;
-	}
-	return false;
+	return Number(param) >= 1 && Number(param) <= lastPokedexEntry;
 };
