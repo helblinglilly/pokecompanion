c309afc3e3340d8ac77691a86f922f990895f0f9
Joel Helbling <helblingjoel@gmail.com>
2023-10-14 12:17:51 +0100

    Add placeholder logout confirm

---
+++ src/routes/auth/logout/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { pb } from '$lib/pocketbase';
+	import { currentUser } from '$lib/stores/user';
+
+	pb.authStore.clear();
+	$currentUser = null;
+
+	setTimeout(() => {
+		goto('/');
+	}, 1000);
+</script>
+
+<p>You have been signed out</p>
