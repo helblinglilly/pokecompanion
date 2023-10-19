297baea4d7a8f415da05b466f766355c56dc395d
Joel Helbling <helblingjoel@gmail.com>
2023-10-19 23:51:20 +0100

    FIx bug

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	import { currentUser } from '$lib/stores/user';
+	import Image from '$components/Image.svelte';
 </script>
 
-<p>Hello signed in user</p>
+<div class="columns">
+	<div class="column">
+		<div class="card">
+			{#if $currentUser.avatar}
+				<Image src={$currentUser.avatar} alt="Profile Picture" />
+			{:else}
+				<p>{JSON.stringify($currentUser)}</p>
+			{/if}
+
+			<!-- <Image src={$currentUser.avatar} alt="Profile Picture" /> -->
+		</div>
+	</div>
+</div>
