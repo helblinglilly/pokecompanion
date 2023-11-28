854fb256bd2430ef5a3467a2aecc5c19c73330f3
Joel Helbling <helblingjoel@gmail.com>
2023-11-28 22:46:29 +0000

    Adjust test

--- e2e/auth/login.test.ts
+++ e2e/auth/login.test.ts
@@ -13,22 +13,6 @@ test('Email/Password flow validates user input and allows sign-up correctly', as
 		'Must contain: 1 uppercase, 1 lowercase, 1 number, 1 special char and be 8+ characters long'
 	);
 	expect(errorText).toBeTruthy();
-
-	await page.getByLabel('Password', { exact: true }).fill('P@assword1!');
-	await page.getByRole('button', { name: 'Sign up' }).click();
-
-	await page.waitForFunction(() => document.readyState === 'complete');
-	await page.getByLabel('Confirm Password').fill('P@assword1!');
-	await page
-		.getByLabel('Username (Optional)')
-		.fill('cool username with spaces (invalid characters)');
-
-	await page.getByRole('button', { name: 'Sign up' }).click();
-
-	await page.waitForFunction(() => document.readyState === 'complete');
-
-	const signupFailText = page.locator('#wrapper').getByText('Sign up failed');
-	expect(signupFailText).toBeTruthy();
 });
 
 // Playwright doesn't support dealing with multipart form data...
