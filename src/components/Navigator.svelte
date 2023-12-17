ea62f0358e41730088723a4d0befc8f05e7753c4
Joel Helbling <helblingjoel@gmail.com>
2023-12-17 14:52:23 +0000

    Add variety and form on mon page

--- src/components/Navigator.svelte
+++ src/components/Navigator.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { pokemonDisplayStore } from '$lib/stores/pokemonPageStore';
+	import { capitaliseFirstLetter } from '$lib/utils/string';
 	import Image from './Image.svelte';
 
 	export let title: string;
@@ -6,6 +10,7 @@
 	export let maxId: number;
 	export let iconUrl: string | undefined;
 	export let route: string;
+	export let forms: { name: string }[];
 </script>
 
 <div id="navigator" class="columns mobile">
@@ -34,10 +39,43 @@
 			#{currentId}
 		</h2>
 
-		<h2 class="desktop-only" id="pokemonName">
-			{title}
-		</h2>
+		<div class="desktop-only">
+			<div style="display: inline-block; gap: 1rem;">
+				<h2
+					id="pokemonName"
+					style="min-width: fit-content; margin-top: auto; margin-bottom: auto; padding-bottom: 0;"
+				>
+					{title}
+				</h2>
+
+				{#if forms.length > 1}
+					<select
+						name="variety"
+						style="min-width: 10rem; margin: 0;"
+						on:change={(e) => {
+							if (e.target) {
+								const newUrl = new URL($page.url);
+								if (e.target.value.endsWith('-default')) {
+									newUrl.searchParams.delete('variety');
+									goto(newUrl.toString(), { replaceState: true });
+								} else {
+									newUrl.searchParams.set('variety', e.target.value);
+									goto(newUrl.toString(), { replaceState: true });
+								}
+							}
+						}}
+					>
+						{#each forms as form}
+							<option value={form.name} selected={form.name === $pokemonDisplayStore.variety?.name}
+								>{capitaliseFirstLetter(form.name.split('-').splice(1).join(' '))}</option
+							>
+						{/each}
+					</select>
+				{/if}
+			</div>
+		</div>
 	</div>
+
 	<div class="column" style="justify-content: end; display: contents; max-width: fit-content;">
 		{#if currentId + 1 <= maxId}
 			<a href={`${route}/${currentId + 1}`} class="card" style="width: fit-content; padding: 10px;">
@@ -63,6 +101,32 @@
 	<div class="columns" style="justify-content: space-between;">
 		<div class="column">
 			<h2 style="text-align: center;">{title}</h2>
+
+			{#if forms.length > 1}
+				<div style="display: inline-flex; width: 100%; justify-content: space-around;">
+					<select
+						name="variety"
+						on:change={(e) => {
+							if (e.target) {
+								const newUrl = new URL($page.url);
+								if (e.target.value.endsWith('-default')) {
+									newUrl.searchParams.delete('variety');
+									goto(newUrl.toString(), { replaceState: true });
+								} else {
+									newUrl.searchParams.set('variety', e.target.value);
+									goto(newUrl.toString(), { replaceState: true });
+								}
+							}
+						}}
+					>
+						{#each forms as form}
+							<option value={form.name} selected={form.name === $pokemonDisplayStore.variety?.name}
+								>{capitaliseFirstLetter(form.name.split('-').splice(1).join(' '))}</option
+							>
+						{/each}
+					</select>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
--- src/routes/pokemon/[slug=pokedexid]/+page.server.ts
+++ src/routes/pokemon/[slug=pokedexid]/+page.server.ts
@@ -1,7 +1,7 @@
 import { findGameFromString } from '$lib/data/games.js';
 import { getPokemonTypesInGame, getTypeRelations } from '$lib/data/generationAdjuster.js';
 import { pokeApiDomain } from '$lib/stores/domain';
-import type { IPokemon, IPokemonSpecies } from '$lib/types/IPokemon';
+import type { IPokemon, IPokemonSpecies, ISprites, Name } from '$lib/types/IPokemon';
 import { speciesNamesToNormalisedNames } from '$lib/utils/language';
 import { capitaliseFirstLetter, pokemonVarietyNameToDisplay } from '$lib/utils/string';
 
@@ -17,6 +17,17 @@ const loadPokemonSpecies = async (id: number): Promise<IPokemonSpecies> => {
 	return body;
 };
 
+const loadPokemonForm = async (
+	url: string
+): Promise<{
+	sprites: ISprites;
+	names: Name[];
+}> => {
+	const response = await fetch(url);
+	const body = await response.json();
+	return body;
+};
+
 export const load = async ({ params, url, cookies }) => {
 	const pokedexId = Number(params.slug);
 	const gameEntry = findGameFromString(cookies.get('selectedGame'));
@@ -27,34 +38,80 @@ export const load = async ({ params, url, cookies }) => {
 		loadPokemonSpecies(pokedexId)
 	]);
 
-	const varietyEntry = species.varieties.find(
-		(entry) => entry.pokemon.name === url.searchParams.get('variety')
-	);
+	const requestedVariety = url.searchParams.get('variety');
+
+	const formEntry = pokemon.forms.find((entry) => entry.name === requestedVariety);
+	if (formEntry) {
+		try {
+			const formData = await loadPokemonForm(formEntry.url);
+			pokemon = {
+				...pokemon,
+				sprites: formData.sprites
+			};
+
+			species = {
+				...species,
+				names: formData.names
+			};
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	const varietyEntry = species.varieties.find((entry) => entry.pokemon.name === requestedVariety);
+
 	if (varietyEntry) {
 		try {
 			const parts = varietyEntry.pokemon.url.split('/');
 			const id = Number(parts[parts.length - 2]);
-			const varietyPokemon = await loadPokemon(id);
+			let varietyPokemon = await loadPokemon(id);
 
 			const nameParts = varietyPokemon.name.split('-');
-			const varietyName = pokemonVarietyNameToDisplay(varietyPokemon.name);
 
 			if (!nameParts) {
 				throw new Error(`Tried to process an invalid variety of a Pokemon`);
 			}
-			varietyPokemon.name = varietyName + ' ' + capitaliseFirstLetter(nameParts[0]);
+
+			const varietyForm = varietyPokemon.forms.find((a) => a.name === varietyPokemon.name);
+			const varietyName = pokemonVarietyNameToDisplay(varietyPokemon.name);
+
+			if (varietyForm) {
+				const varietyFormPokemon = await loadPokemonForm(varietyForm?.url);
+				const hasNewSprites = Object.keys(varietyFormPokemon.sprites).some((key) => {
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore Literally iterating over the keys
+					return varietyFormPokemon.sprites[key] !== null;
+				});
+
+				varietyPokemon = {
+					...varietyPokemon,
+					...varietyFormPokemon,
+					sprites: hasNewSprites ? varietyFormPokemon.sprites : varietyPokemon.sprites
+				};
+
+				if (varietyFormPokemon.names.length) {
+					species.names = varietyFormPokemon.names;
+				} else {
+					species.names = species.names.map((name) => {
+						return {
+							language: name.language,
+							name: varietyName + ' ' + capitaliseFirstLetter(nameParts[0])
+						};
+					});
+				}
+			} else {
+				species.names = species.names.map((name) => {
+					return {
+						language: name.language,
+						name: varietyName + ' ' + capitaliseFirstLetter(nameParts[0])
+					};
+				});
+			}
 
 			pokemon = {
 				...pokemon,
 				...varietyPokemon
 			};
-
-			species.names = species.names.map((name) => {
-				return {
-					language: name.language,
-					name: varietyPokemon.name
-				};
-			});
 		} catch (err) {
 			console.error(err);
 		}
@@ -64,7 +121,24 @@ export const load = async ({ params, url, cookies }) => {
 
 	return {
 		id: pokedexId,
-		pokemon: { ...pokemon, types, typeRelations: await getTypeRelations(types[0], types[1]) },
+		pokemon: {
+			...pokemon,
+			types,
+			typeRelations: await getTypeRelations(types[0], types[1]),
+			varietyForms: species.varieties
+				.map((variety) => {
+					return {
+						name: variety.pokemon.name.includes('-')
+							? variety.pokemon.name
+							: variety.pokemon.name + '-default'
+					};
+				})
+				.concat(pokemon.forms)
+				.filter(
+					(entry, index, arr) =>
+						entry.name.includes('-') && arr.findIndex((e) => e.name === entry.name) === index
+				)
+		},
 		species: {
 			...species,
 			names: speciesNamesToNormalisedNames(species.names)
