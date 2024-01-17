1211574ce438c37229c37d941766717d68d4354d
Joel Helbling <helblingjoel@gmail.com>
2024-01-17 23:51:24 +0000

    Adjust layout

--- src/components/Pokemon/Moveset.svelte
+++ src/components/Pokemon/Moveset.svelte
@@ -7,50 +7,60 @@
 	$: relevantMoves = allMoves[0];
 </script>
 
-<div class="columns">
+<div class="container">
 	{#if relevantMoves.levelupMoves.length > 0}
-		<div class="column">
+		<div class="item">
 			<h4>Level up</h4>
 			{#each relevantMoves.levelupMoves as levelMove}
 				<Move url={levelMove.move.url} level={levelMove.level} />
 			{/each}
 		</div>
 	{:else}
-		<div class="column" />
+		<div class="item" />
 	{/if}
 
 	{#if relevantMoves.tmMoves.length > 0}
-		<div class="column">
+		<div class="item">
 			<h4>TM / HM</h4>
 			{#each relevantMoves.tmMoves as tmMove}
 				<Move url={tmMove.move.url} />
 			{/each}
 		</div>
 	{:else}
-		<div class="column" />
+		<div class="item" />
 	{/if}
-</div>
 
-<div class="columns">
 	{#if relevantMoves.breedMoves.length > 0}
-		<div class="column">
+		<div class="item">
 			<h4>Breeding</h4>
 			{#each relevantMoves.breedMoves as breedMove}
 				<Move url={breedMove.move.url} />
 			{/each}
 		</div>
 	{:else}
-		<div class="column" />
+		<div class="item" />
 	{/if}
 
 	{#if relevantMoves.tutorMoves.length > 0}
-		<div class="column">
+		<div class="item">
 			<h4>Tutored</h4>
 			{#each relevantMoves.tutorMoves as tutorMove}
 				<Move url={tutorMove.move.url} />
 			{/each}
 		</div>
 	{:else}
-		<div class="column" />
+		<div class="item" />
 	{/if}
 </div>
+
+<style>
+	.container {
+		min-width: 100%;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		grid-gap: 2rem;
+	}
+
+	.item {
+	}
+</style>
