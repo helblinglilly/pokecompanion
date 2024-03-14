8328ee9ef9060d3826d5046c701f9ff6c1f2a31b
Lilly Helbling <lilly@helbling.uk>
2024-03-14 20:48:38 +0000

    Include url with social previews

--- src/components/SocialPreview.svelte
+++ src/components/SocialPreview.svelte
@@ -4,6 +4,7 @@
 	export let description =
 		'Explore the world of the Pokémon video games without the information overload. Set your game and never get lost in the flood of information again!';
 	export let previewImage = '/socialpreview/generic.png';
+	import { page } from '$app/stores';
 </script>
 
 <svelte:head>
@@ -17,12 +18,12 @@
 
 	<meta property="og:type" content="website" />
 
-	<meta property="og:url" content="https://pokecompanion.helbling.uk" />
-	<meta property="twitter:url" content="https://pokecompanion.helbling.uk" />
-	<meta property="twitter:domain" content="pokecompanion.helbling.uk" />
+	<meta property="og:url" content={$page.url.origin} />
+	<meta property="twitter:url" content={$page.url.origin} />
+	<meta property="twitter:domain" content={$page.url.hostname} />
 
 	<meta name="twitter:card" content="summary_large_image" />
 
-	<meta property="og:image" content={previewImage} />
-	<meta name="twitter:image" content={previewImage} />
+	<meta property="og:image" content={`${$page.url.origin}${previewImage}`} />
+	<meta name="twitter:image" content={`${$page.url.origin}${previewImage}`} />
 </svelte:head>
