3bfd3f94a3a62bd167ec21f6180d620948e05187
Lilly Helbling <lilly@helbling.uk>
2024-03-28 22:32:56 +0000

    Properly sign a user out

--- src/routes/auth/logout/+page.svelte
+++ src/routes/auth/logout/+page.svelte
@@ -2,13 +2,15 @@
 	import { goto } from '$app/navigation';
 	import { pb } from '$lib/stores/domain';
 	import { currentUser } from '$lib/stores/user';
-	import { deleteCookie } from '$lib/utils/cookies';
+	import { deleteCookie, setCookie } from '$lib/utils/cookies';
 	import { onMount } from 'svelte';
+	import { v4 as uuid } from 'uuid';
 
 	$pb.authStore.clear();
 	$currentUser = null;
 	deleteCookie('pb_auth');
 	deleteCookie('auth-redirect');
+	setCookie('remember-token', uuid());
 
 	onMount(() => {
 		goto('/auth/signin');
