24a67e4de3752f79320c7934e78f3f34b8fa778d
Lilly Helbling <lilly@helbling.uk>
2024-03-28 18:16:55 +0000

    Move type

--- src/routes/api/helpers.ts
+++ src/routes/api/helpers.ts
@@ -1,10 +1,11 @@
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
-import { findGameFromString, type IGame } from '$lib/data/games';
+import { findGameFromString } from '$lib/data/games';
 import { warn } from '$lib/log';
 import { getRawCookie, parseCookieString } from '$lib/utils/cookies';
 import { addMinutesToDate } from '$lib/utils/date';
 import type { Cookies } from '@sveltejs/kit';
 import Pocketbase from 'pocketbase';
+import type { IUserPreferences } from './types';
 
 export const getSearchParam = (url: string, name: string) => {
 	const searchParts = url.split('?')[1];
@@ -101,12 +102,6 @@ export const respondWithJson = (payload: object | Array<unknown>, status?: numbe
 		status: responseCode
 	});
 };
-
-export interface IUserPreferences {
-	primaryLanguage: string;
-	secondaryLanguage: string | undefined;
-	selectedGame: IGame | undefined;
-}
 
 export const parseUserPreferences = (url: URL, cookies: Cookies): IUserPreferences => {
 	const gameEntry = findGameFromString(url.searchParams.get('game') ?? cookies.get('game'));
--- src/routes/api/types.ts
+++ src/routes/api/types.ts
@@ -1,6 +1,14 @@
+import type { IGame } from "$lib/data/games";
+
 export interface Platform {
 	context: {
 		waitUntil(promise: Promise<unknown>): void;
 	}
 	caches: CacheStorage & { default: Cache }
+}
+
+export interface IUserPreferences {
+	primaryLanguage: string;
+	secondaryLanguage: string | undefined;
+	selectedGame: IGame | undefined;
 }
