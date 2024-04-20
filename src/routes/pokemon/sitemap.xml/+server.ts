60ae99ead1ab38d9f7c2b4c1337998ffa622fae1
Lilly Helbling <lilly@helbling.uk>
2024-04-20 20:42:26 +0100

    Add basic sitemaps for main pages and /pokemon/:id

---
+++ src/routes/pokemon/sitemap.xml/+server.ts
@@ -0,0 +1,28 @@
+import { lastPokedexEntry } from "$lib/stores/domain";
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async () => {	
+    const validPokemon = [];
+
+    for (let i = 1; i < lastPokedexEntry; i++){
+        validPokemon.push(i);
+    }
+
+    const sitemap = `
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${validPokemon.map(id => `
+                <url>
+                    <loc>https://pokecompanion.com/pokemon/${id}</loc>
+                    <changefreq>monthly</changefreq>
+                    <priority>0.5</priority>
+                </url>
+            `).join('')}
+        </urlset>
+    `;
+
+    return new Response(sitemap, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
+};
