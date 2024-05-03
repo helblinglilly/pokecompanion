6e6084fff917076e4cea19741886b4b2197dd283
Lilly Helbling <lilly@helbling.uk>
2024-05-03 19:21:07 +0100

    Disable caching

--- src/routes/api/pokemon/cachedFetch.ts
+++ src/routes/api/pokemon/cachedFetch.ts
@@ -4,11 +4,13 @@ import { pokeApiDomain } from "$lib/stores/domain";
 import { type IPokemon, type IPokemonSpecies, type ISprites, type Name, emptySprites } from "$lib/types/IPokemon";
 import type { Platform } from "../types";
 
+const isCachingEnabled = false;
+
 export const fetchCacheFirst = async(url: string | URL, platform: Readonly<Platform> | undefined): Promise<Response> => {
 	const parsedUrl = new URL(url);
 	const req = new Request(parsedUrl);
 
-	if (platform?.caches?.default){
+	if (platform?.caches?.default && isCachingEnabled){
 		try {
 			const cacheResponse = await platform.caches.default.match(url);
 			if (cacheResponse){
@@ -28,7 +30,7 @@ export const fetchCacheFirst = async(url: string | URL, platform: Readonly<Platf
 	}
 
 	const res = await fetch(req);
-	if (res.ok && platform?.caches?.default){
+	if (res.ok && platform?.caches?.default && isCachingEnabled){
 		try {
 			const responseToCache = res.clone();
             platform.context.waitUntil(
