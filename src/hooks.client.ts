ded3bc6a5277988e936e1bc95d280a70b2d53b14
Lilly Helbling <lilly@helbling.uk>
2024-05-04 12:48:18 +0100

    Sentry - don't log local client side errors

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,12 @@
+import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 import * as Sentry from '@sentry/browser';
 import type { HandleServerError } from '@sveltejs/kit';
 export const handleError: HandleServerError = async ({ error, event, status, message }) => {
 	const errorId = crypto.randomUUID();
 
-	Sentry.captureException(error);
+	if (PUBLIC_ENVIRONMENT !== 'local'){
+		Sentry.captureException(error);
+	}
 
 	console.log(error, event);
 
