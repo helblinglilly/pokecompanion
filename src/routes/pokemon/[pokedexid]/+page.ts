7ff18964775311d324e252fe9e1303ca0bb0283f
Lilly Helbling <lilly@helbling.uk>
2024-05-25 22:44:57 +0100

    Pass animation down properly
    And only pass down the game if the sprite setting lines up with it

--- src/routes/pokemon/[pokedexid]/+page.ts
+++ src/routes/pokemon/[pokedexid]/+page.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { IPokemonResponse } from '../../api/pokemon/types';
 import { get } from 'svelte/store';
-import { selectedGame, primaryLanguage, secondaryLanguage } from '$lib/stores/domain';
+import { selectedGame, primaryLanguage, secondaryLanguage, animateSprites, versionSpecificSprites } from '$lib/stores/domain';
 import { getGameGroupFromName, PokeapiVersionGroups } from '$lib/data/games';
 
 export const load = async ({ params, fetch, url }) => {
@@ -12,9 +12,11 @@ export const load = async ({ params, fetch, url }) => {
 	requestUrl.searchParams.append('variety', url.searchParams.get('variety') ?? '');
 	requestUrl.searchParams.append('shiny', url.searchParams.get('shiny') ?? '');
 	requestUrl.searchParams.append('gender', url.searchParams.get('gender') ?? '');
+	requestUrl.searchParams.append('animateSprites', url.searchParams.get('animateSprites') ?? `${get(animateSprites)}`);
 	const game = getGameGroupFromName(url.searchParams.get('game') as PokeapiVersionGroups ?? get(selectedGame)?.pokeapi)
+	const showGameSpecificSprites = url.searchParams.get('versionSpecificSprites') ?? get(versionSpecificSprites);
 
-	if (game){
+	if (game && showGameSpecificSprites){
 		requestUrl.searchParams.append('game', game.pokeapi)
 	}
 
