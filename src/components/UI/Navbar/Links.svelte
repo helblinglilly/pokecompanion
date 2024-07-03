587c5cc458bf6664a04dcb6e8896af3542c59197
Lilly Helbling <lilly@helbling.uk>
2024-07-03 23:27:58 +0100

    Rebuild navbar
    Some colours are still wrong in dark theme

---
+++ src/components/UI/Navbar/Links.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { theme } from '$/lib/stores/domain';
+	import { currentUser } from '$/lib/stores/user';
+</script>
+
+<a href="/pokemon" class="h-full">
+	<button class="w-48 h-12 md:w-fit md:px-4">
+		<p class="grid h-full align-center items-center">Pokémon</p>
+	</button>
+</a>
+
+<a href="/settings" class="h-full">
+	<button class="w-48 h-12 md:w-fit md:px-4">
+		<p class="grid h-full align-center items-center">Settings</p>
+	</button>
+</a>
+
+<a href="/about" class="h-full">
+	<button class="w-48 h-12 md:w-fit md:px-4">
+		<p class="grid h-full align-center items-center">About</p>
+	</button>
+</a>
+
+{#if $currentUser}
+	<a href={`/user/${$currentUser.username}`} class="h-full">
+		<button class="w-48 h-12 md:w-fit md:px-4">
+			<p class="grid h-full align-center items-center">Me - {$currentUser.username}</p>
+		</button>
+	</a>
+{/if}
+
+<a href={$currentUser ? '/auth/logout' : '/auth/signin'} class="h-full">
+	<button class="w-48 h-12 md:w-fit md:px-4">
+		<p class="grid h-full align-center items-center">Sign {$currentUser ? 'Out' : 'In'}</p>
+	</button>
+</a>
+
+<button
+	class="w-48 h-12 md:w-fit md:px-4"
+	on:click={() => {
+		if ($theme === 'light') {
+			theme.set('dark');
+		} else {
+			theme.set('light');
+		}
+	}}><p class="grid h-full align-center items-center">Theme</p></button
+>
+
+<style>
+	p {
+		color: var(--light);
+		width: 100%;
+	}
+
+	button {
+		display: inline-flex;
+	}
+
+	button:hover {
+		background-color: var(--dark);
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class', // Enable dark mode with the 'class' strategy
 	theme: {
-		extend: {}
+	  extend: {
+		colors: {
+			primary: {
+				DEFAULT: 'var(--primary)',
+				dark: 'var(--primary)'
+			},
+			secondary: {
+				DEFAULT: 'var(--secondary)',
+				dark: 'var(--secondary)'
+			},
+			accent: {
+				DEFAULT: 'var(--accent)',
+				dark: 'var(--accent)'
+			},
+			textColour: {
+				DEFAULT: 'var(--dark)',
+				dark: 'var(--light)'
+			},
+			textInverse: {
+				DEFAULT: 'var(--light)',
+				dark: 'var(--dark)'
+			},
+			navbar: {
+				DEFAULT: 'var(--branding-secondary)'
+			},
+			dark: {
+				DEFAULT: 'var(--dark)'
+			}
+		},
+	  },
 	},
-	plugins: []
-};
+	plugins: [],
+  };
