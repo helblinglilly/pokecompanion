fa2034970a37408185508d05ca6582c511a7c7a2
Lilly Helbling <lilly@helbling.uk>
2024-07-13 15:11:48 +0100

    Progress on storybook

---
+++ .storybook/BackgroundDecorator.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '$/styles/global.css';
+</script>
+
+<div style="padding-top: 2rem; padding-bottom: 2rem; padding-left: 1rem; padding-right: 1rem;">
+	<slot />
+</div>
---
+++ src/ui/organisms/Navbar/NavbarLinks/NavbarLinks.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { theme } from '$/lib/stores/domain';
+	import { currentUser } from '$/lib/stores/user';
+</script>
+
+<a href="/pokemon" class="w-32 h-12 md:w-fit md:px-4">
+	<p class="grid h-full text-center items-center">Pokémon</p>
+</a>
+
+<a href="/settings" class="w-32 h-12 md:w-fit md:px-4">
+	<p class="grid h-full text-center items-center">Settings</p>
+</a>
+
+<a href="/about" class="w-32 h-12 md:w-fit md:px-4">
+	<p class="grid h-full text-center items-center">About</p>
+</a>
+
+{#if $currentUser}
+	<a href={`/user/${$currentUser.username}`} class="w-32 h-12 md:w-fit md:px-4">
+		<p class="grid h-full text-center items-center">Me - {$currentUser.username}</p>
+	</a>
+{/if}
+
+<a href={$currentUser ? '/auth/logout' : '/auth/signin'} class="w-32 h-12 md:w-fit md:px-4">
+	<p class="grid h-full text-center items-center">Sign {$currentUser ? 'Out' : 'In'}</p>
+</a>
+
+<button
+	class="w-32 h-12 md:w-fit md:px-4 md:ml-auto"
+	on:click={() => {
+		if ($theme === 'light') {
+			theme.set('dark');
+		} else {
+			theme.set('light');
+		}
+	}}><p class="grid h-full align-center items-center">Theme</p></button
+>
+
+<style>
+	p {
+		color: #edebeb;
+		width: 100%;
+	}
+
+	a {
+		display: inline-flex;
+		background-color: #3d3938;
+	}
+
+	a:hover,
+	button:hover {
+		background-color: #3d3938;
+	}
+</style>
