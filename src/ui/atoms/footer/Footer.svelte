edc4888fbe29f89658cead0d67c7a3410403f4c0
Lilly Helbling <lilly@helbling.uk>
2024-07-15 18:43:03 +0100

    Rejig Footer and add kofi

--- src/ui/atoms/footer/Footer.svelte
+++ src/ui/atoms/footer/Footer.svelte
@@ -1,19 +1,40 @@
 <footer>
-	<p>Powered by <a href="https://pokeapi.co" class="underline">PokéAPI</a></p>
-	<p>
-		Built at <a href="https://github.com/helblinglilly/pokecompanion" class="underline"
-			>github.com/helblinglilly/pokecompanion</a
-		>
-	</p>
+	<div class="columns gap-2">
+		<div class="column grid justify-center md:block md:text-left">
+			<div class="w-full text-center">
+				<a href="/about">About</a>
+			</div>
+			<div class="w-full text-center">
+				<a href="/privacy/policy">Privacy Policy</a>
+			</div>
+		</div>
+		<div class="column grid text-center gap-2">
+			<p>Powered by <a href="https://pokeapi.co" class="underline">PokéAPI</a></p>
+			<p>
+				Built at <a href="https://github.com/helblinglilly/pokecompanion" class="underline"
+					>github.com/helblinglilly/pokecompanion</a
+				>
+			</p>
+			<a href="https://ko-fi.com/X8X710K2JH" target="_blank" class="grid justify-center"
+				><img
+					style="border:0px;height:24px;"
+					src="/kofi_button_red.png"
+					alt="Buy Me a Coffee at ko-fi.com"
+				/></a
+			>
+		</div>
 
-	<p>Pokémon and Pokémon character names are trademarks of Nintendo.</p>
-	<p>This site is not associated with Nintendo, Gamefreak, The Pokémon Company or PokéAPI</p>
+		<div class="column">
+			<p>Pokémon and Pokémon character names are trademarks of Nintendo.</p>
+			<p>This site is not associated with Nintendo, Gamefreak, The Pokémon Company or PokéAPI</p>
+		</div>
+	</div>
 </footer>
 
 <style>
 	footer {
 		display: grid;
-		text-align: center;
+		/* text-align: center; */
 		padding: 0.5rem;
 		background-color: #3d3938d2;
 		position: relative;
