a25e2f882bbfb8b0618eccf85bcd914302cd97a2
Lilly Helbling <lilly@helbling.uk>
2024-09-14 11:14:57 +0100

    Fix layouts for Navigator
    Give up and use JS because we don't want to render two h1s for SEO
    reasons|

--- src/routes/pokemon/[pokedexid]/Navigator.svelte
+++ src/routes/pokemon/[pokedexid]/Navigator.svelte
@@ -10,52 +10,55 @@
 	export let title: string;
 	export let currentId: number;
 	export let forms: { name: string }[];
+
+	$: innerWidth = 750;
 </script>
 
+<svelte:window bind:innerWidth />
+
 <div class="grid gap-2">
-	<div class="flex justify-between" style="margin-bottom: 0; padding-bottom: 0;">
-		<div class="max-w-fit contents">
+	{#if innerWidth < 450}
+		<div class="w-full grid gap-2">
+			<h1 class="h2 text-center">{title}</h1>
+			<div class="inline-flex w-full justify-between gap-4">
+				<NavigationButton pokedexId={currentId - 1} />
+				<h2 class="h2 my-auto">#{currentId}</h2>
+				<NavigationButton pokedexId={currentId + 1} />
+			</div>
+		</div>
+	{:else}
+		<div class="w-full inline-flex justify-between gap-2">
 			<NavigationButton pokedexId={currentId - 1} />
-		</div>
-
-		<div class="grid items-center max-w-fit gap-4">
-			<h1 class={`h2 grid ${forms.length > 1 ? 'mt-4' : 'my-auto'} pb-0 text-center`}>
-				#{currentId}
-				{title}
-			</h1>
-
-			{#if forms.length > 1}
-				<Select
-					value={$pokemonDisplayStore.variety}
-					options={forms.map((form) => ({
-						label: capitaliseFirstLetter(form.name.split('-').splice(1).join(' ')),
-						value: form.name
-					}))}
-					style="width: 100%; padding-left: 1rem; padding-right: 1rem; margin: 0; text-align: center;"
-					on:change={({ detail }) => {
-						Logger.addPageAction('UIInteraction', 'Variety', {
-							action: 'Navigation'
-						});
-
-						const newUrl = new URL($page.url);
-
-						if (detail.endsWith('-default')) {
-							newUrl.searchParams.delete('variety');
-							goto(newUrl.toString());
-						} else {
-							newUrl.searchParams.set('variety', detail);
-							goto(newUrl.toString());
-						}
-					}}
-				/>
-			{/if}
-		</div>
-
-		<div
-			class="justify-end contents max-w-fit"
-			style="justify-content: end; display: contents; max-width: fit-content;"
-		>
+			<h1 class="h2 my-auto text-center">#{currentId} {title}</h1>
 			<NavigationButton pokedexId={currentId + 1} />
 		</div>
-	</div>
+	{/if}
+
+	{#if forms.length > 1}
+		<div class="w-full flex justify-center">
+			<Select
+				value={$pokemonDisplayStore.variety}
+				options={forms.map((form) => ({
+					label: capitaliseFirstLetter(form.name.split('-').splice(1).join(' ')),
+					value: form.name
+				}))}
+				style="width: 100%; padding-left: 1rem; padding-right: 1rem; margin: 0; text-align: center;"
+				on:change={({ detail }) => {
+					Logger.addPageAction('UIInteraction', 'Variety', {
+						action: 'Navigation'
+					});
+
+					const newUrl = new URL($page.url);
+
+					if (detail.endsWith('-default')) {
+						newUrl.searchParams.delete('variety');
+						goto(newUrl.toString());
+					} else {
+						newUrl.searchParams.set('variety', detail);
+						goto(newUrl.toString());
+					}
+				}}
+			/>
+		</div>
+	{/if}
 </div>
