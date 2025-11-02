9780ff9c0fc5ede2fdce19d2048a0460b5622d48
Lilly Helbling <lilly@helbling.uk>
2025-11-02 19:35:35 +0000

    Delete the deletion endpoint
    This has been migrated to the new API

--- src/routes/api/tag/[tagId]/+server.ts
+++ src/routes/api/tag/[tagId]/+server.ts
@@ -1,43 +1,9 @@
-import { Logger } from "$/lib/log";
-import isStringToxic from "$/lib/server/toxic";
-import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
+import { Logger } from '$/lib/log';
+import isStringToxic from '$/lib/server/toxic';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import Pocketbase from 'pocketbase';
-import { validateAuth } from "../../helpers";
-import type { ITagDatabase, ITagMeta } from "../types";
-
-/**
- * Delete a specific tag
- */
-export async function DELETE({ request, cookies, platform, params }) {
-	const authedPb = await validateAuth(request, cookies);
-
-	if (!authedPb) {
-		return new Response('Not authorised', { status: 401 });
-	}
-
-	try {
-		await authedPb.collection('tags').delete(params.tagId);
-	} catch (err) {
-		platform?.context.waitUntil(
-			Logger.error(
-				Logger.ErrorClasses.TagOperation,
-				Logger.buildError(err),
-				{
-					context: 'Deleting a tag',
-					errorMessage: 'DB Operation: Failed to delete a tag',
-					user: cookies.get('remember-token'),
-					tags: params.tagId
-				}
-			)
-		)
-
-		return new Response('Failed to delete tag', {
-			status: 500
-		});
-	}
-
-	return new Response(`Deleted ${params.tagId}`);
-}
+import { validateAuth } from '../../helpers';
+import type { ITagDatabase, ITagMeta } from '../types';
 
 /**
  * Update meta information about a tag
@@ -52,50 +18,46 @@ export async function PATCH({ request, cookies, platform, params }) {
 	let body: Partial<ITagMeta & { content: any }> | undefined;
 	try {
 		body = await request.json();
-	} catch(err){
+	} catch (err) {
 		platform?.context.waitUntil(
-			Logger.error(
-				Logger.ErrorClasses.TagOperation,
-				Logger.buildError(err),
-				{
-					context: 'Patching meta data about a tag',
-					errorMessage: 'Failed to parse JSON from request body',
-					user: cookies.get('remember-token')
-				}
-			)
-		)	
+			Logger.error(Logger.ErrorClasses.TagOperation, Logger.buildError(err), {
+				context: 'Patching meta data about a tag',
+				errorMessage: 'Failed to parse JSON from request body',
+				user: cookies.get('remember-token')
+			})
+		);
 
-		return new Response('Invalid body', { status: 400 })
+		return new Response('Invalid body', { status: 400 });
 	}
 
-	if (!body){
+	if (!body) {
 		return new Response('Empty body', { status: 400 });
 	}
 
 	const toxicPromises: Promise<boolean>[] = [];
-	if (body.name){
-		toxicPromises.push(isStringToxic(body.name))
+	if (body.name) {
+		toxicPromises.push(isStringToxic(body.name));
 	}
-	if (body.description){
-		toxicPromises.push(isStringToxic(body.description))
+	if (body.description) {
+		toxicPromises.push(isStringToxic(body.description));
 	}
 
-	if (toxicPromises.length > 0){
+	if (toxicPromises.length > 0) {
 		const toxicResults = await Promise.all(toxicPromises);
-		if (toxicResults.some((result) => result)){
+		if (toxicResults.some((result) => result)) {
 			platform?.context.waitUntil(
 				Logger.info('Caught Toxic tag entry', {
 					context: 'Patch tag',
 					name: body.name,
 					description: body.description
-				} )
-			)
+				})
+			);
 			return new Response('Internal server error', { status: 500 });
 		}
 	}
 
 	const withoutContent = { ...body };
-	if (typeof withoutContent['content'] !== undefined){
+	if (typeof withoutContent['content'] !== undefined) {
 		delete withoutContent.content;
 	}
 
@@ -103,38 +65,30 @@ export async function PATCH({ request, cookies, platform, params }) {
 		await authedPb.collection('tags').update(params.tagId, {
 			...withoutContent
 		});
-	} catch(err){
+	} catch (err) {
 		platform?.context.waitUntil(
-			Logger.error(
-				Logger.ErrorClasses.TagOperation,
-				Logger.buildError(err),
-				{
-					context: 'Patching meta data about a tag',
-					errorMessage: 'DB Operation: Failed to update the tag',
-					user: cookies.get('remember-token')
-				}
-			)
-		)		
+			Logger.error(Logger.ErrorClasses.TagOperation, Logger.buildError(err), {
+				context: 'Patching meta data about a tag',
+				errorMessage: 'DB Operation: Failed to update the tag',
+				user: cookies.get('remember-token')
+			})
+		);
 
-		return new Response('Internal server error', { status: 500 })
+		return new Response('Internal server error', { status: 500 });
 	}
 
 	try {
 		const fullUpdatedTag = await authedPb.collection('tags').getOne(params.tagId);
-		return new Response(JSON.stringify(fullUpdatedTag), { status: 200 })
-	} catch(err){
+		return new Response(JSON.stringify(fullUpdatedTag), { status: 200 });
+	} catch (err) {
 		platform?.context.waitUntil(
-			Logger.error(
-				Logger.ErrorClasses.TagOperation,
-				Logger.buildError(err),
-				{
-					context: 'Patching meta data about a tag',
-					errorMessage: 'Tag has been updated but failed to retrieve it for confirmation',
-					user: cookies.get('remember-token')
-				}
-			)
-		)		
-		return new Response(JSON.stringify({}), { status: 200 })
+			Logger.error(Logger.ErrorClasses.TagOperation, Logger.buildError(err), {
+				context: 'Patching meta data about a tag',
+				errorMessage: 'Tag has been updated but failed to retrieve it for confirmation',
+				user: cookies.get('remember-token')
+			})
+		);
+		return new Response(JSON.stringify({}), { status: 200 });
 	}
 }
 
@@ -150,26 +104,22 @@ export async function GET({ request, cookies, platform, params }) {
 	}
 
 	try {
-		const tags = await pb.collection('tags').getOne(params.tagId) as ITagDatabase
+		const tags = (await pb.collection('tags').getOne(params.tagId)) as ITagDatabase;
 
 		return new Response(JSON.stringify(tags), {
-			status: 200,
-		})
+			status: 200
+		});
 	} catch (err) {
 		platform?.context.waitUntil(
-			Logger.error(
-				Logger.ErrorClasses.TagOperation,
-				Logger.buildError(err),
-				{
-					context: 'Failed to get a specific tag',
-					user: cookies.get('remember-token'),
-					tagId: params.tagId
-				}
-			)
-		)
+			Logger.error(Logger.ErrorClasses.TagOperation, Logger.buildError(err), {
+				context: 'Failed to get a specific tag',
+				user: cookies.get('remember-token'),
+				tagId: params.tagId
+			})
+		);
 
 		return new Response('Internal error', {
 			status: 500
-		})
+		});
 	}
 }
