a560149daa0662723359032ae34998403b051d62
Lilly Helbling <lilly@helbling.uk>
2025-11-08 23:22:23 +0000

    Ensure query params get passed

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,15 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, cookies }) => {
 	return {
-		user: locals.user
+		user: locals.user,
+		preferences: {
+			animateSprites: cookies.get('animateSprites'),
+			primaryLanguage: cookies.get('primaryLanguage'),
+			secondaryLanguage: cookies.get('secondaryLanguage'),
+			selectedGame: cookies.get('selectedGame'),
+			versionSpecificPokemonSprites: cookies.get('versionSpecificPokemonSprites'),
+			versionSpecificTypeSprites: cookies.get('versionSpecificTypeSprites')
+		}
 	};
 };
--- src/routes/pokemon/+page.ts
+++ src/routes/pokemon/+page.ts
@@ -1,7 +1,9 @@
 import { PUBLIC_API_HOST } from '$env/static/public';
 import type { APIPokemonRootPreview } from '$/@types/api.pokecompanion';
 
-export async function load({ url, fetch }) {
+export async function load({ url, fetch, parent }) {
+	const { preferences } = await parent();
+
 	const apiRequest = new URL(`${PUBLIC_API_HOST}/pokemon/preview`);
 
 	const jumpTo = url.searchParams.get('jumpTo');
@@ -17,6 +19,31 @@ export async function load({ url, fetch }) {
 	const pageSize = url.searchParams.get('pageSize');
 	if (pageSize) {
 		apiRequest.searchParams.append('pageSize', pageSize);
+	}
+
+	if (preferences.animateSprites) {
+		apiRequest.searchParams.append('animateSprites', preferences.animateSprites);
+	}
+	if (preferences.primaryLanguage) {
+		apiRequest.searchParams.append('primaryLanguage', preferences.primaryLanguage);
+	}
+	if (preferences.secondaryLanguage) {
+		apiRequest.searchParams.append('secondaryLanguage', preferences.secondaryLanguage);
+	}
+	if (preferences.selectedGame) {
+		apiRequest.searchParams.append('selectedGame', preferences.selectedGame);
+	}
+	if (preferences.versionSpecificPokemonSprites) {
+		apiRequest.searchParams.append(
+			'versionSpecificPokemonSprites',
+			preferences.versionSpecificPokemonSprites
+		);
+	}
+	if (preferences.versionSpecificTypeSprites) {
+		apiRequest.searchParams.append(
+			'versionSpecificTypeSprites',
+			preferences.versionSpecificTypeSprites
+		);
 	}
 
 	const res = await fetch(apiRequest);
