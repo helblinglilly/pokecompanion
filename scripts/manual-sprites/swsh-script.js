8088feb81527777df5298918ecddeec45661585e
Lilly Helbling <lilly@helbling.uk>
2025-12-20 12:09:10 +0000

    Bump node

--- scripts/manual-sprites/swsh-script.js
+++ scripts/manual-sprites/swsh-script.js
@@ -1,4 +1,4 @@
-import fs from "fs"
+import fs from 'fs';
 import path from 'path';
 
 // https://projectpokemon.org/home/docs/spriteindex_148/3d-models-generation-8-pok%C3%A9mon-r123/
@@ -7,85 +7,84 @@ const inputDirectory = 'scripts/manual-sprites/sword-shield-input';
 const outputDir = 'scripts/manual-sprites/sword-shield-output';
 
 fs.readdir(outputDir, (err, files) => {
-    if (err) {
-        console.error(err);
-    } else {
-        files.forEach((file) => {
-            fs.unlink(path.join(outputDir, file), (err) => {
-                if (err) {
-                    console.error(err);
-                }
-            });
-        });
-    }
+	if (err) {
+		console.error(err);
+	} else {
+		files.forEach((file) => {
+			fs.unlink(path.join(outputDir, file), (err) => {
+				if (err) {
+					console.error(err);
+				}
+			});
+		});
+	}
 });
 
-async function getPokemonName(id){ 
-    return await fetch(`https://pokecompanion.com/api/pokemon/${id}`).then(async (res) => {
-        if (res.ok){
-            const data = await res.json();
-            return `${data.pokemon.name}`;
-        }
-        return 'unknown'
-    })
+async function getPokemonName(id) {
+	return await fetch(`https://pokecompanion.com/api/pokemon/${id}`).then(async (res) => {
+		if (res.ok) {
+			const data = await res.json();
+			return `${data.pokemon.name}`;
+		}
+		return 'unknown';
+	});
 }
 
 fs.readdir(inputDirectory, (err, files) => {
-    files.forEach(async (file) => {
-            const fileExtension = path.extname(file);
-    
-            if (fileExtension === '.gif'){
-                let baseFilename = file.replace("gigantamax", "gmax").split('.gif')[0];
-    
-                const parts = baseFilename.split('-');
-                let pokedex =parseInt(parts[0]);
-                let others =  parts.slice(2);
+	files.forEach(async (file) => {
+		const fileExtension = path.extname(file);
 
-                let filename = `${pokedex}`;
+		if (fileExtension === '.gif') {
+			let baseFilename = file.replace('gigantamax', 'gmax').split('.gif')[0];
 
-                const variants = others.filter((entry) => !['s', 'shiny', 'f', 'female'].includes(entry));
+			const parts = baseFilename.split('-');
+			let pokedex = parseInt(parts[0]);
+			let others = parts.slice(2);
 
-                if (variants.length > 0){
-                    const pokemonName = await getPokemonName(pokedex);
-                    filename += '-' + pokemonName + '-' + variants.join('-');
-                }
+			let filename = `${pokedex}`;
 
-                if (others.some((entry) => entry === 'f' || entry === 'female')){
-                    filename += '-female'
-                }
-                if (others.some((entry) => entry === 's' || entry === 'shiny')){
-                    filename += '-shiny'
-                }
+			const variants = others.filter((entry) => !['s', 'shiny', 'f', 'female'].includes(entry));
 
-                let newFilename = `${filename}.gif`
+			if (variants.length > 0) {
+				const pokemonName = await getPokemonName(pokedex);
+				filename += '-' + pokemonName + '-' + variants.join('-');
+			}
 
+			if (others.some((entry) => entry === 'f' || entry === 'female')) {
+				filename += '-female';
+			}
+			if (others.some((entry) => entry === 's' || entry === 'shiny')) {
+				filename += '-shiny';
+			}
 
-                if (pokedex <= 890 && newFilename.includes()){
-                    fs.copyFile(path.join(inputDirectory, file), path.join(outputDir, newFilename), (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
-                }
-            }
-    });
+			let newFilename = `${filename}.gif`;
+
+			if (pokedex <= 890 && newFilename.includes()) {
+				fs.copyFile(path.join(inputDirectory, file), path.join(outputDir, newFilename), (err) => {
+					if (err) {
+						console.error(err);
+					}
+				});
+			}
+		}
+	});
 });
 
 const maxId = 1025;
 
 let missing = [];
 
-for (let i = 1; i <= maxId; i++){
-    fs.readdir(outputDir, (err, files) => {
-        const hasMatching = files.some((file) => {
-            const pokedex = file.split('.')[0].split('-')[0];
-            return Number(pokedex) === i;
-        })
+for (let i = 1; i <= maxId; i++) {
+	fs.readdir(outputDir, (err, files) => {
+		const hasMatching = files.some((file) => {
+			const pokedex = file.split('.')[0].split('-')[0];
+			return Number(pokedex) === i;
+		});
 
-        if (!hasMatching){
-            missing.push(i);
-        }
-    })
+		if (!hasMatching) {
+			missing.push(i);
+		}
+	});
 }
 
 console.log('Missing Pokemon', missing);
