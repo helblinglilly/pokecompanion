1f554e1ee05f37b4fc135428ca617924a161d867
Lilly Helbling <lilly@helbling.uk>
2026-06-15 20:41:58 +0100

    Add app invite page

---
+++ src/routes/app/invite/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { addNotification } from '$/features/notifications/notifications';
+	import Button from '$/ui/atoms/Button.svelte';
+	import { PUBLIC_API_HOST } from '$env/static/public';
+
+	let email = $state('');
+
+	const onSubmit = async () => {
+		const res = await fetch(`${PUBLIC_API_HOST}/app/invite`, {
+			method: 'POST',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email: email,
+				service: 'google'
+			})
+		});
+
+		if (!res.ok) {
+			addNotification({ message: 'Sorry, please try that again', level: 'failure' });
+			return;
+		}
+		addNotification({ message: 'Thanks, well be in touch soon!', level: 'success' });
+		email = '';
+	};
+</script>
+
+<div class="justify-center grid gap-8">
+	<h1 class="h1">Register your interest in the Pokécompanion App!</h1>
+
+	<h2 class="h2">We are actively looking for testers on Android/Google Play</h2>
+
+	<form onsubmit={onSubmit} class="grid gap-4">
+		<p>Use the form below to sign up as a beta tester. You should receive an invite within 72h.</p>
+		<textarea maxlength="64" bind:value={email} placeholder={'test@example.com'}></textarea>
+
+		<Button variant="accent" type="submit">Register</Button>
+	</form>
+
+	<p>Please note that invitations are currently restricted to the UK, Germany and Switzerland</p>
+</div>
