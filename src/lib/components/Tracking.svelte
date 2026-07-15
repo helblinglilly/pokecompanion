de55428e75dbc292345fd7898f54d2da139b808f
Lilly Helbling <lilly@helbling.uk>
2026-07-15 22:45:30 +0100

    Track core web vitals

--- src/lib/components/Tracking.svelte
+++ src/lib/components/Tracking.svelte
@@ -8,6 +8,9 @@
 			defer
 			src="https://analytics.helbling.uk/script.js"
 			data-website-id="f303cb13-d1aa-42c0-ab5b-77937e5a1daa"
+			data-domains="pokecompanion.com,www.pokecompanion.com"
+			data-do-not-track="true"
+			data-performance="true"
 		></script>
 	{/if}
 </svelte:head>
